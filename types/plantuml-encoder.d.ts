0460759699edbb646d157f05489bbf4a5f88dd8f
Слава Титов <slavatitov171102@gmail.com>
2026-01-01 11:24:42 +0500

    Add TypeScript declarations for plantuml-encoder

---
+++ types/plantuml-encoder.d.ts
@@ -0,0 +1,10 @@
+declare module 'plantuml-encoder' {
+  interface PlantUmlEncoder {
+    encode(text: string): string;
+    decode(encoded: string): string;
+  }
+  
+  const plantumlEncoder: PlantUmlEncoder;
+  export = plantumlEncoder;
+}
+
